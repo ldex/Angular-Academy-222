011d9f562c0319ee004a73eebeed6911885acb06
ldex <laurent.duveau+git@gmail.com>
2020-12-09 18:44:01 -0500

    End of Day 2

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product.service';
+import { threadId } from 'worker_threads';
 import { Product } from '../product.interface';
 
 @Component({
@@ -8,11 +13,39 @@ import { Product } from '../product.interface';
 })
 export class ProductDetailComponent implements OnInit {
 
-  @Input() product: Product;
+  product$: Observable<Product>;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) { }
+
+  delete(id: number): void {
+    if(window.confirm('Are you sure ??')) {
+      this
+      .productService
+      .deleteProduct(id)
+      .subscribe(
+        () => {
+          console.log('Product was deleted');
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        }
+      )
+    }
+  }
 
   ngOnInit(): void {
+    let id = + this.activatedRoute.snapshot.params['id'];
+
+    this.product$ = this
+                    .productService
+                    .products$
+                    .pipe(
+                      map(products => products.find(product => product.id === id))
+                    ); 
+
   }
 
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -8,23 +10,68 @@ import { Product } from '../product.interface';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   title: string = 'Products';
  // products: Product[];
   products$: Observable<Product[]>;
+  productsNb$: Observable<number>;
   selectedProduct: Product;
+  errorMessage: string;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage(): void {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+  nextPage(): void {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
 
   onSelect(product: Product) {
     this.selectedProduct = product;
+    this.router.navigateByUrl('/products/' + product.id);
   }
 
-  constructor(private productService: ProductService) { 
+  constructor(
+    private productService: ProductService,
+    private router: Router) { 
+    
+  }
+
+  ngOnDestroy() {
     
   }
 
   ngOnInit(): void {
-    this.products$ = this.productService.products$;
+    this.products$ = this
+                        .productService
+                        .products$
+                        .pipe(
+                          catchError(
+                            error => {
+                              this.errorMessage = error;
+                              return EMPTY;
+                            }
+                          )
+                        );
+
+    this.productsNb$ = this
+                        .products$
+                        .pipe(
+                          map(products => products.length),
+                          startWith(0)
+                        );
 
     // this
     //   .productService
